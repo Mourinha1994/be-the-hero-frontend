ed82cfa0269c83a0a91c783a1212b8565a7da98e
Erick Moura <erickmelodemoura17@gmail.com>
2020-03-28 16:04:13 -0300

    Handle login

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -8,14 +10,33 @@ import heroImg from '../../assets/heroes.png';
 import  logoImg from  '../../assets/logo.svg';
 
 export default function Logon () {
+
+    const [id, setId ] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(event) {
+        event.preventDefault();
+
+        try {
+            const response = await api.post('/sessions', { id });
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile')
+        } catch (error) {
+            alert(error);
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Logo"/>
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu Logon</h1>
-                    <input placeholder="Sua ID"/>
+                    <input placeholder="Sua ID" value={id} onChange={e => setId(e.target.value)}/>
                     <button className="button" type="submit">Entrar</button>
 
                     <Link className="back-link" to="/register">
