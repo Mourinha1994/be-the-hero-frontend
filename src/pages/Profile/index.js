6d74bdb6ae97d06a07535c59579a09cf86d598cb
Erick Moura <erickmelodemoura17@gmail.com>
2020-03-28 16:50:01 -0300

    Profile and Register fixes

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -17,7 +17,7 @@ export default function Profile () {
     const ongName = localStorage.getItem('ongName');
 
     useEffect(() => {
-        api.get('/profiles', {
+        api.get('/incidents', {
             headers: {
                 authorization: ongId
             }
